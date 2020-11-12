9aa62cc6fb17de12a2d982dc9ed708bfde7235f4
MaykiSantos <58126683+MaykiSantos@users.noreply.github.com>
2020-11-11 23:30:43 -0300

    envio
    envio arquivos

---
+++ js/principal.js
@@ -0,0 +1,10 @@
+$('.autoplay').slick({
+    dots: true,
+    infinite: true,
+    centerMode: true,
+    variableWidth: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
